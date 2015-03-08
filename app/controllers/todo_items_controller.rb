6e2978cfe0aa87484e7cd73ff7330d2e97ce4bac
cmthoman <epimyth@gmail.com>
2015-03-07 22:31:33 -0500

    fixed create method in TodoItemsController

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -9,5 +9,20 @@ class TodoItemsController < ApplicationController
   end
 
   def create
+  	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new(todo_item_params)
+  	if @todo_item.save
+  		flash[:success] = "Added todo list item."
+  		redirect_to todo_list_todo_items_path
+  	else
+  		flash[:error] = "There was a problem adding that todo list item."
+  		render action: :new
+  	end
   end
+
+  private
+  def todo_item_params
+  	params.require(:todo_item).permit(:content)
+  end
+
 end
